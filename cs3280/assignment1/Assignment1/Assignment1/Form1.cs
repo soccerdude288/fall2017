443451c48a3e83a10355b124df8f78493d10ff22
Taylor Earl <W01186504@cs.weber.edu>
2017-09-02 12:45:54 -0600

    C# Assignment 1

--- cs3280/assignment1/Assignment1/Assignment1/Form1.cs
+++ cs3280/assignment1/Assignment1/Assignment1/Form1.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * Taylor Earl
+ * CS3280
+ * 9/2/17
+ */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +21,49 @@ namespace Assignment1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult MyResult;
+
+            MyResult = MessageBox.Show("You typed: " + textBox1.Text, "This Caption", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+            result.Text = "You clicked the " + MyResult.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult MyResult;
+
+            MyResult = MessageBox.Show("You typed: " + textBox2.Text, "This Caption", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+
+            result.Text = "You clicked the " + MyResult.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult MyResult;
+
+            MyResult = MessageBox.Show("You typed: " + textBox3.Text, "This Caption", MessageBoxButtons.YesNo, MessageBoxIcon.Hand);
+
+            result.Text = "You clicked the " + MyResult.ToString();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
