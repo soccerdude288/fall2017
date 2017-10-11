7d9bfa01d5721f65a7bd47c41e657fb6c00a97b3
Taylor Earl <taylor@Chriss-MBP.veracitynetworks.com>
2017-10-10 21:56:50 -0600

    android a7

---
+++ cs3270/CS3270A7/app/src/main/java/com/example/taylor/cs3270a7/CourseListFragment.java
@@ -0,0 +1,44 @@
+package com.example.taylor.cs3270a7;
+
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CourseListFragment extends Fragment {
+
+
+    public CourseListFragment() {
+        // Required empty public constructor
+    }
+
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view =  inflater.inflate(R.layout.fragment_course_list, container, false);
+
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        return view;
+    }
+
+}
---
+++ cs3270/CS3270A7/app/src/main/java/com/example/taylor/cs3270a7/DatabaseHelper.java
@@ -0,0 +1,8 @@
+package com.example.taylor.cs3270a7;
+
+/**
+ * Created by taylor on 10/10/17.
+ */
+
+public class DatabaseHelper {
+}
