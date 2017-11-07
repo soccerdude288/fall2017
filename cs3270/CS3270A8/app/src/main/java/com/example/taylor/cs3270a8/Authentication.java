94f3271b6af6718a82ad2f550cc73c1339726153
Taylor <soccerdude28844@gmail.com>
2017-11-07 00:09:44 -0700

    Final Andorid

--- cs3270/CS3270A8/app/src/main/java/com/example/taylor/cs3270a8/Authentication.java
+++ cs3270/CS3270A8/app/src/main/java/com/example/taylor/cs3270a8/Authentication.java
@@ -6,5 +6,5 @@ package com.example.taylor.cs3270a8;
 
 public final class Authentication {
 //INSERT CUSTOM CANVAS KEY
-    final static String KEY = "";
+    final static String KEY = "14~dAaM5QkpTfE99euddQWahVR7y9hV3Nz9HJT2IzxCD4mmqS6xst3uYWQ32fWdI4NA";
 }
